32ec9aed115f76881769e2959f9ec8cf3c7a3e26
Popelnitskyi_Maxim <popelnitskyi@gmail.com>
2023-05-14 16:09:20 +0300

    update bat

--- migrate.bat
+++ migrate.bat
@@ -1,5 +1,4 @@
-dotnet build --project WMS.Data\WMS.Data.csproj --startup-project WMS.Data\WMS.Data.csproj
-dotnet ef database drop --project WMS.Data\WMS.Data.csproj --startup-project WMS.Data\WMS.Data.csproj
+dotnet ef database drop --force --project WMS.Data\WMS.Data.csproj --startup-project WMS.Data\WMS.Data.csproj
 dotnet ef migrations remove --project WMS.Data\WMS.Data.csproj --startup-project WMS.Data\WMS.Data.csproj
 dotnet ef migrations add InitialCreate  --project WMS.Data\WMS.Data.csproj --startup-project WMS.Data\WMS.Data.csproj
 dotnet ef database update --project WMS.Data\WMS.Data.csproj --startup-project WMS.Data\WMS.Data.csproj
