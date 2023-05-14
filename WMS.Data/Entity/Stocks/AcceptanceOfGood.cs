49fea1725b409df7c49299b9bae059e19d592e4c
Alarak666 <popelnitskyi70@gmail.com>
2023-05-14 16:08:43 +0300

    create db

--- WMS.Data/Entity/Stocks/AcceptanceOfGood.cs
+++ WMS.Data/Entity/Stocks/AcceptanceOfGood.cs
@@ -6,12 +6,12 @@ namespace WMS.Data.Entity.Stocks
 {
     public class AcceptanceOfGood : BaseCatalog
     {
-        public Guid TypePalletId { get; set; }
-        public Pallet TypePallet { get; set; }
-        public Guid ProductId { get; set; }
-        public Product Product { get; set; }
-        public Guid EmployerId { get; set; }
-        public Employee Employer { get; set; }
+        public Guid? TypePalletId { get; set; }
+        public Pallet? TypePallet { get; set; }
+        public Guid? ProductId { get; set; }
+        public Product? Product { get; set; }
+        public Guid? EmployerId { get; set; }
+        public Employee? Employer { get; set; }
         public double Width { get; set; }
         public double Height { get; set; }
         public double Length { get; set; }
@@ -19,6 +19,6 @@ namespace WMS.Data.Entity.Stocks
         public int Qty { get; set; }
         public DateTime DateAccepts { get; set; }
         public DateTime DataExpiration { get; set; }
-        public string NPallet { get; set; }
+        public string NPallet { get; set; } = string.Empty;
     }
 }
